ad5c4537e9013b968d9ec5b2fa3ed31acf6fe896
mathengerennison015@gmail.com <mathengerennison015@gmail.com>
2024-10-30 15:26:43 +0300

    changes made, added function, calculates, function examples

---
+++ functionexample.py
@@ -0,0 +1,11 @@
+def emobilisrubyclass (name,age,gender):
+    print(f"studentname is : {name} Age: {age} and Gender: {gender}")
+emobilisrubyclass("Rennison", 23,"male")
+emobilisrubyclass("Lucy",20, "female")
+
+def btit(course,regno, year):
+    course = input("Enter your course :")
+    regno = input(("Enter your registration number: "))
+    year = int(input(("Enter your year: ")))
+    print(f"I am taking {course} course and my registration number is {regno} and year of admission is {year}")
+btit("Bachelorof technology Information Technology","SCII00618/2020",2020)
---
+++ functions.py
@@ -0,0 +1,9 @@
+def rennison():
+    print("welcome to python function concepts")
+rennison()
+
+def calculatetwo():
+    num = int(input("Enter a first number: "))
+    num2 = int(input("Enter a second number: "))
+    print(f"The sum of {num} and {num2} is {num + num2}")
+calculatetwo()
